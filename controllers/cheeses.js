9519ffd864af82bb2b76b21a83f0b628a45c5686
Sam <sam.ryan101@gmail.com>
2023-08-17 11:18:59 +0100

    finished tests for the getbytype

--- controllers/cheeses.js
+++ controllers/cheeses.js
@@ -5,13 +5,24 @@ const CheeseController = {
     GetByType: async (req, res) => {
         const { type } = req.params;
         try {
-            const cheese_results = await CheeseSchema.find({
-                "attributes.types": type,
-            }).lean();
-            const CheeseCleaners = cheese_results.map((cheese) => {
-                return new CheeseCleaner(cheese);
-            });
-            res.status(200).json(CheeseCleaners);
+            if (type === "all") {
+                const all_cheese_results = await CheeseSchema.find({}).lean();
+                const CheeseCleaners = all_cheese_results.map((cheese) => {
+                    return new CheeseCleaner(cheese);
+                });
+                return res.status(200).json(CheeseCleaners);
+            } else {
+                const cheese_results = await CheeseSchema.find({
+                    "attributes.types": type,
+                }).lean();
+                if (cheese_results.length === 0) {
+                    return res.status(404).json("Cheese type not found");
+                }
+                const CheeseCleaners = cheese_results.map((cheese) => {
+                    return new CheeseCleaner(cheese);
+                });
+                res.status(200).json(CheeseCleaners);
+            }
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
