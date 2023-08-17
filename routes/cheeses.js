78721cac92886c8b50df9ee3fcfc5561154a4b95
Sam <130215065+iamsahm@users.noreply.github.com>
2023-08-17 11:42:26 +0100

    Update cheeses.js
    cheesecontroller

--- routes/cheeses.js
+++ routes/cheeses.js
@@ -6,6 +6,6 @@ const CheesesController = require("../controllers/cheeses");
 
 router.get("/random", CheesesController.Random);//index cheese random pic
 router.get("/:id", CheesesController.CheeseById);
-router.get("/type/:type", CheeseController.GetByType);
+router.get("/type/:type", CheesesController.GetByType);
 
 module.exports = router;
